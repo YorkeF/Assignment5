5c03d84c47919b72ffc2b2e79067f583a527e4d4
YorkeF <113220697+YorkeF@users.noreply.github.com>
2022-11-02 22:51:35 -0500

    Assignment 5

--- src/Ferrell_ProblemSentimentAnalysis.java
+++ src/Ferrell_ProblemSentimentAnalysis.java
@@ -66,16 +66,20 @@ public class Ferrell_ProblemSentimentAnalysis {
         int b = 1;
         for (int i = 1; i < list.size(); i++) {
             if(b == 2628 || b == 2746 || b == 4044 || b == 4365 || b == 4761 || b == 5695 || b == 5916 || b == 6231 || b == 6358 || b == 6673 || b == 6922  || b == 7325 || b == 7473 || b == 8443 || b == 8530){
-                b++;//What the fuck.
+                entries[b] = new ReviewEntry();
+                b++;
             }
+            //Creates the Main Object
             if(Integer.parseInt(list.get(i)[1]) == b) {
                 entries[b] = new ReviewEntry(list.get(i));
                 b++;
                 if(b == 8545){
                     break;
                 }
+
+            //Adds phrases to the Main Object
             }else {
-                entries[b].addAPhrase(list.get(i));
+                entries[b - 1].addAPhrase(list.get(i));
             }
         }
 
